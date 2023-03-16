7122cdd93a9a92737ccc86a3d39f09ae91cf261b
Syi-I <ryang28975@gmail.com>
2023-03-16 12:32:53 -0500

    rotating block base

--- src/main/java/com/uberhelixx/flatlights/block/RotatingBlock.java
+++ src/main/java/com/uberhelixx/flatlights/block/RotatingBlock.java
@@ -1,2 +1,50 @@
-package com.uberhelixx.flatlights.block;public class RotatingBlock {
+package com.uberhelixx.flatlights.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.DirectionalBlock;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.item.BlockItemUseContext;
+import net.minecraft.state.StateContainer;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockReader;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
+
+import java.util.function.ToIntFunction;
+
+public class RotatingBlock extends DirectionalBlock {
+
+    static final float BLOCK_HARDNESS = 0.4f;
+    static final float BLOCK_RESISTANCE = 100000000f;
+    public static ToIntFunction<BlockState> ROTATING_BLOCK_LIGHT_LEVEL = BlockState -> 15;
+
+    public RotatingBlock() {
+        super(Properties.create(Material.GLASS)
+                .hardnessAndResistance(BLOCK_HARDNESS, BLOCK_RESISTANCE)
+                .setLightLevel(ROTATING_BLOCK_LIGHT_LEVEL)
+                .notSolid()
+                .setOpaque(RotatingBlock::isntSolid)
+                .sound(SoundType.GLASS));
+    }
+
+    protected void fillStateContainer(StateContainer.Builder<Block, BlockState> builder) {
+        builder.add(FACING);
+    }
+
+    public BlockState getStateForPlacement(BlockItemUseContext context) {
+        return this.getDefaultState().with(FACING, context.getPlacementHorizontalFacing().getOpposite());
+    }
+
+    private static boolean isntSolid(BlockState state, IBlockReader reader, BlockPos pos) {
+        return false;
+    }
+
+    @Override
+    @OnlyIn(Dist.CLIENT)
+    public float getAmbientOcclusionLightValue(BlockState state, IBlockReader worldIn, BlockPos pos) { return 1.0F; }
+
+    @Override
+    public boolean propagatesSkylightDown(BlockState state, IBlockReader reader, BlockPos pos) { return true; }
 }
