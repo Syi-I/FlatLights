f02327a22b6086e3dc21cefd5245601d37956483
Syi-I <ryang28975@gmail.com>
2024-03-19 22:43:27 -0500

    - new VoidProjectileRenderer model -> uses smaller sphere similar to VoidSphereRenderer now, instead of shitty looking arrow model with black square texture

--- src/main/java/com/uberhelixx/flatlights/render/VoidProjectileRenderer.java
+++ src/main/java/com/uberhelixx/flatlights/render/VoidProjectileRenderer.java
@@ -1,13 +1,28 @@
 package com.uberhelixx.flatlights.render;
 
+import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.vertex.IVertexBuilder;
 import com.uberhelixx.flatlights.FlatLights;
 import com.uberhelixx.flatlights.entity.VoidProjectileEntity;
+import com.uberhelixx.flatlights.entity.VoidSphereEntity;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.BlockRendererDispatcher;
+import net.minecraft.client.renderer.IRenderTypeBuffer;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.ArrowRenderer;
+import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
+import net.minecraft.client.renderer.model.IBakedModel;
+import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.vector.Vector3f;
+import net.minecraftforge.client.model.data.EmptyModelData;
 
-public class VoidProjectileRenderer extends ArrowRenderer<VoidProjectileEntity> {
-    public static final ResourceLocation projectile = new ResourceLocation(FlatLights.MOD_ID, "textures/entity/void_projectile.png");
+import java.awt.*;
+
+public class VoidProjectileRenderer extends EntityRenderer<VoidProjectileEntity> {
+    public static final ResourceLocation SPHERE = new ResourceLocation(FlatLights.MOD_ID, "textures/models/void_sphere/void_sphere.png");
+    public static final ResourceLocation SPHERE_MODEL = new ResourceLocation(FlatLights.MOD_ID, "entity/void_sphere_wrapper");
 
     //render like an arrow would be
     public VoidProjectileRenderer(EntityRendererManager renderManagerIn) {
@@ -15,7 +30,42 @@ public class VoidProjectileRenderer extends ArrowRenderer<VoidProjectileEntity> 
     }
 
     @Override
+    public void render(VoidProjectileEntity entityIn, float entityYaw, float partialTicks, MatrixStack matrixStackIn, IRenderTypeBuffer bufferIn, int packedLightIn) {
+        IBakedModel sphereModel = Minecraft.getInstance().getModelManager().getModel(SPHERE_MODEL);
+
+        matrixStackIn.push();
+        MatrixStack.Entry currentMatrix = matrixStackIn.getLast();
+
+        final float MODEL_SIZE_IN_ORIGINAL_COORDINATES = 1.0F;  // size of the wavefront model
+        final float TARGET_SIZE_WHEN_RENDERED = 0.05F;  // desired size when rendered (in metres)
+
+        final float SCALE_FACTOR = TARGET_SIZE_WHEN_RENDERED / MODEL_SIZE_IN_ORIGINAL_COORDINATES;
+        matrixStackIn.scale(SCALE_FACTOR, SCALE_FACTOR, SCALE_FACTOR);
+        matrixStackIn.translate(0, 0.75, 0);
+        matrixStackIn.rotate(Vector3f.YP.rotationDegrees(-1 * entityIn.ticksExisted));
+        float flicker = entityIn.ticksExisted % 2 * 0.08f;
+        matrixStackIn.scale(1 - flicker, 1 - flicker, 1 - flicker);
+
+        Color blendColour = Color.WHITE;
+        float red = blendColour.getRed() / 255.0F;
+        float green = blendColour.getGreen() / 255.0F;
+        float blue = blendColour.getBlue() / 255.0F;
+
+        // we're going to use the block renderer to render our model, even though it's not a block, because we baked
+        //   our entity model as if it were a block model.
+        BlockRendererDispatcher dispatcher = Minecraft.getInstance().getBlockRendererDispatcher();
+
+        IVertexBuilder vertexBuffer = bufferIn.getBuffer(RenderType.getSolid());
+        dispatcher.getBlockModelRenderer().renderModel(currentMatrix, vertexBuffer, null, sphereModel,
+                red, green, blue, packedLightIn, OverlayTexture.NO_OVERLAY, EmptyModelData.INSTANCE);
+
+        matrixStackIn.pop(); // restore the original transformation matrix + normals matrix
+
+        super.render(entityIn, entityYaw, partialTicks, matrixStackIn, bufferIn, packedLightIn);  // renders labels
+    }
+
+    @Override
     public ResourceLocation getEntityTexture(VoidProjectileEntity entity) {
-        return projectile;
+        return SPHERE;
     }
 }
